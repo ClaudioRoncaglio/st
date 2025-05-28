311370582673d216889ed7f4edcb2a75d2476428
ClaudioRoncaglio <claudio.roncaglio@gmail.com>
2025-05-28 10:41:38 +0200

    Impostato Blex Mono Nerd Font come font principale

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "IBMPlexMono-Regular:pixelsize=16:antialias=true:autohint=true";
+static char *font = "BlexMonoNerdFont:pixelsize=16:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 	"FontAwesome6Free-Regular:pixelsize=16:antialias=true:autohint=true",
